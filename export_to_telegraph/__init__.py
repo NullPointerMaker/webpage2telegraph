fe6bf4f29e6a57bf083ee1d3bee2b9bcedc4a9c2
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-11-17 04:54:59 -0800

    commit

--- export_to_telegraph/__init__.py
+++ export_to_telegraph/__init__.py
@@ -24,7 +24,7 @@ OFFTOPIC_ATT = [
 	'story-body__unordered-list', 'story-image-copyright', 'article-header', 
 	'top-wrapper', 'bottom-of-article', 'bottom-wrapper', 'linkList', 
 	'display:none;', 'accordion', 'el-editorial-source', 'video__end-slate__tertiary-title',
-	'adblocker',
+	'adblocker', 'tagline'
 ]
 
 OFFTOPIC_CLASSES = ['ads']
@@ -133,6 +133,8 @@ def _getInnerArticle(soup):
 	return soup
 
 def _isOffTopic(attrs):
+	if matchKey(attrs, ['tagline']):
+		print(attrs)
 	if matchKey(attrs, OFFTOPIC_ATT):
 		return True
 	if 'hidden' in attrs and not matchKey(attrs, ['lazy', 'false']):
@@ -258,7 +260,20 @@ def _getCaption(item):
 def _copyB(soup):
 	return BeautifulSoup(str(soup), features="lxml")
 
+def _findnoscriptImg(img):
+	if not img.parent:
+		return
+	noscript = img.parent.find('noscript')
+	if not noscript:
+		return
+	return noscript.find('img')
+
 def _cleanupImages(soup, domain):
+	for img in soup.find_all('img'):
+		noscript_img = _findnoscriptImg(img)
+		if noscript_img:
+			img.replace_with(noscript_img)
+
 	for figure in soup.find_all('figure'):
 		r = _cleanupFigure(figure, domain)
 		if r: 
@@ -321,9 +336,9 @@ def saveSoup(soup, stage):
 def _findTextFromSoup(soup, url):
 	domain = _findDomain(soup, url)
 	saveSoup(soup, 0)
-	soup = _decomposeOfftopic(soup, url)
-	saveSoup(soup, 1)
 	soup = _cleanupImages(soup, domain)
+	saveSoup(soup, 1)
+	soup = _decomposeOfftopic(soup, url)
 	saveSoup(soup, 2)
 	soup = _getInnerArticle(soup)
 	saveSoup(soup, 3)
@@ -462,21 +477,22 @@ def export(url, throw_exception=False, force=False):
 			raise e
 
 urls = [
-	# 'https://t.co/k2kLBpdQhl',
-	# 'https://t.co/4ik2VsUHeB',
-	# 'https://edition.cnn.com/2019/11/11/asia/mouse-deer-vietnam-chevrotain-rediscovered-scn/index.html',
-	# 'https://www.pinknews.co.uk/2019/11/14/same-sex-marriage-in-sweden-and-denmark-has-reduced-the-number-of-lesbians-and-gay-men-dying-by-suicide-by-almost-half/?fbclid=IwAR2Rq8aPs7lACGJOmC_N549Px9QvZAYGeCjd8_Z-i5owBlLKbtX7UyGm4l8',
-	# 'https://www.nytimes.com/2019/10/10/opinion/sunday/feminism-lean-in.html',
-	# 'https://gen.medium.com/everyones-missing-the-obvious-when-it-comes-to-the-declining-u-s-birth-rate-679abebb854b',
-	# 'https://mp.weixin.qq.com/s/cJLQFljjbT0NzaiMR801aA',
-	# 'https://www.telegraph.co.uk/global-health/women-and-girls/dumped-babies-just-tip-iceberg-deadly-consequences-curbing-reproductive/?fbclid=IwAR0uwFvu3QjbhnYyMxfeN2PtlczcgoiWASrEdRsikQ1Y5TTAO6_PpGH2nDk',
-	# 'https://cn.nytimes.com/china/20191112/hong-kong-protests-volunteer/?utm_source=tw-nytimeschinese&utm_medium=social&utm_campaign=cur',
-	# 'https://telegra.ph/%E9%A6%99%E6%B8%AF%E6%8A%97%E8%AE%AE%E8%80%85%E8%83%8C%E5%90%8E%E7%9A%84%E5%BF%97%E6%84%BF%E8%80%85%E5%A4%A7%E5%86%9B-11-16',
-	# 'https://www.idiva.com/news-opinion/womens-issues/transgender-cabbies-who-are-making-indian-roads-safer-for-women/18004255?fbclid=IwAR3aOtNX0fOukmJ-JNJiImobMfPyVhQ63-i5oEUX38_TRlU4-aBLvHwmaA0',
-	# 'https://www.eurekalert.org/pub_releases/2019-11/lu-ada111519.php',
-	# 'bbc.in/2W2Gohc',
-	# 'https://t.co/Joty1jyQwt',
-	# 'https://www.dw.com/zh/%E6%91%A9%E6%A0%B9%E5%A4%A7%E9%80%9A%E4%B8%80%E5%A4%A7%E9%99%86%E7%B1%8D%E5%91%98%E5%B7%A5%E5%9C%A8%E9%A6%99%E6%B8%AF%E9%81%AD%E6%9A%B4%E6%89%93/a-50723184',
+	# 'https://www.businessinsider.com/trump-other-advertisers-spending-most-on-facebook-political-ads-2019-11?fbclid=IwAR0xfLbdGxBDEBL_WhLQWl8BIUXqEGaw8SP7x6DWSXExClF4x98ZG_w5YCY',
+	'https://t.co/k2kLBpdQhl',
+	'https://t.co/4ik2VsUHeB',
+	'https://edition.cnn.com/2019/11/11/asia/mouse-deer-vietnam-chevrotain-rediscovered-scn/index.html',
+	'https://www.pinknews.co.uk/2019/11/14/same-sex-marriage-in-sweden-and-denmark-has-reduced-the-number-of-lesbians-and-gay-men-dying-by-suicide-by-almost-half/?fbclid=IwAR2Rq8aPs7lACGJOmC_N549Px9QvZAYGeCjd8_Z-i5owBlLKbtX7UyGm4l8',
+	'https://www.nytimes.com/2019/10/10/opinion/sunday/feminism-lean-in.html',
+	'https://gen.medium.com/everyones-missing-the-obvious-when-it-comes-to-the-declining-u-s-birth-rate-679abebb854b',
+	'https://mp.weixin.qq.com/s/cJLQFljjbT0NzaiMR801aA',
+	'https://www.telegraph.co.uk/global-health/women-and-girls/dumped-babies-just-tip-iceberg-deadly-consequences-curbing-reproductive/?fbclid=IwAR0uwFvu3QjbhnYyMxfeN2PtlczcgoiWASrEdRsikQ1Y5TTAO6_PpGH2nDk',
+	'https://cn.nytimes.com/china/20191112/hong-kong-protests-volunteer/?utm_source=tw-nytimeschinese&utm_medium=social&utm_campaign=cur',
+	'https://telegra.ph/%E9%A6%99%E6%B8%AF%E6%8A%97%E8%AE%AE%E8%80%85%E8%83%8C%E5%90%8E%E7%9A%84%E5%BF%97%E6%84%BF%E8%80%85%E5%A4%A7%E5%86%9B-11-16',
+	'https://www.idiva.com/news-opinion/womens-issues/transgender-cabbies-who-are-making-indian-roads-safer-for-women/18004255?fbclid=IwAR3aOtNX0fOukmJ-JNJiImobMfPyVhQ63-i5oEUX38_TRlU4-aBLvHwmaA0',
+	'https://www.eurekalert.org/pub_releases/2019-11/lu-ada111519.php',
+	'bbc.in/2W2Gohc',
+	'https://t.co/Joty1jyQwt',
+	'https://www.dw.com/zh/%E6%91%A9%E6%A0%B9%E5%A4%A7%E9%80%9A%E4%B8%80%E5%A4%A7%E9%99%86%E7%B1%8D%E5%91%98%E5%B7%A5%E5%9C%A8%E9%A6%99%E6%B8%AF%E9%81%AD%E6%9A%B4%E6%89%93/a-50723184',
 ]
 
 def _test():
