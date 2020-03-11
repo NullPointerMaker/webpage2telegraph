e9ece0a2e24a5317e22fba7b10c842a01247bb85
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-11 16:43:37 -0700

    commit

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         'telegram_util>=0.0.31',
         'readee>=0.0.20',
         'hanziconv>=0.3.2',
-        'cached_url>=0.0.4'
+        'cached_url>=0.0.5'
     ],
     python_requires='>=3.0',
 )
