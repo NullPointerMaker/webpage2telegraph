b5b7e44e25933ed8cac8599e09da7f254e74c2d2
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-11 17:27:58 -0700

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="export_to_telegraph",
-    version="0.0.55",
+    version="0.0.56",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Library for export webpage to Telegraph.",
@@ -23,7 +23,7 @@ setuptools.setup(
         'bs4',
         'readability-lxml',
         'telegram_util>=0.0.31',
-        'readee>=0.0.20',
+        'readee>=0.0.24',
         'hanziconv>=0.3.2',
         'cached_url>=0.0.5'
     ],
