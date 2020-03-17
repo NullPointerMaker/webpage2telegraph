f2256cd54b2b31cf9974a97b5592b26b3e2a428a
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-17 13:29:55 -0700

    commit

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 urls = [
-	'https://card.weibo.com/article/m/show/id/2309404477242603339961?_wb_client_=1'
+	'https://telegra.ph/%E6%99%9A%E5%A0%B1%E6%84%8F%E5%AA%92%E6%89%B9%E4%B8%AD%E5%9C%8B%E5%A4%96%E4%BA%A4%E9%83%A8%E5%82%B3%E6%92%AD%E5%81%87%E6%96%B0%E8%81%9E%E5%8F%A6%E6%8C%87%E4%B8%AD%E5%9C%8B%E6%8D%90%E8%B4%88%E7%89%A9%E8%B3%87%E5%AF%A6%E7%82%BA%E6%84%8F%E5%9C%8B%E8%B3%BC%E7%89%A9%E5%95%86%E5%93%81-03-17-2'
 ]
 
 def _test():
