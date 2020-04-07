d91abe5d86f54cbe9241cabf73263f805e27f1d3
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-07 12:02:53 -0700

    commit

--- export_to_telegraph/__init__.py
+++ export_to_telegraph/__init__.py
@@ -121,7 +121,7 @@ def exportAllInText(soup):
 	if not soup:
 		return ''
 	text = str(soup).replace('<br/>', '\n')
-	quote = BeautifulSoup(text, features='lxml').get_text(separator=' ').strip()
+	quote = BeautifulSoup(text, features='lxml').text.strip()
 	for link in soup.find_all('a', title=True, href=True):
 		url = link['title']
 		url = clearUrl(export(url) or url)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="export_to_telegraph",
-    version="0.0.71",
+    version="0.0.72",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Library for export webpage to Telegraph.",
