7ba96f95a8befe636fb716e244911b87500efb9e
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-26 16:58:20 -0700

    commit

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ import sys
 from bs4 import BeautifulSoup
 
 urls = [
-	'https://mp.weixin.qq.com/s?__biz=MzI2MTExMTE3Ng==&amp;mid=2247483688&amp;idx=1&amp;sn=4f1146d79e1d37aa23c61467c876c2a2&amp;chksm=ea5e2dcfdd29a4d96243437cd6f4109925c4e70592e34e12039500c5917412d25174c9d3e885&amp;mpshare=1&amp;scene=1&amp;srcid=&amp;sharer_sharetime=1587789413659&amp;sharer_shareid=7c19efe830ee0c8a2796cbc54e04c282#rd'
+	'https://www.douban.com/group/topic/172663066/?dt_dapp=1'
+
 ]
 
 s = '''
