84bb2a1d4713cd5ab3e87b8220ea0b81da637445
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-06-28 20:54:57 -0700

    commit

--- export_to_telegraph/author.py
+++ export_to_telegraph/author.py
@@ -24,6 +24,7 @@ def _yieldPossibleAuthorItem(soup):
 		yield item.find('p')
 	yield soup.find("a", {"id" : "js_name"})
 	yield soup.find('a', class_='author-url')
+	yield soup.find('span', class_='posted-date')
 		
 def _yieldPossibleOrgItem(soup):
 	yield soup.find("meta", {"property": "twitter:site"})
