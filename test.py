9a03946d29c4379ac9cf3d373f490fe85408588f
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-05-05 12:04:44 -0700

    commit

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ tele = Updater(bot_token, use_context=True)
 chat = tele.bot.get_chat(420074357)
 
 urls = [
-	'https://mp.weixin.qq.com/s/JtESLzGeqqMCu-cmrBuRXQ',
+	'https://mp.weixin.qq.com/s/pgDa7WWWKlCfcT1v2JNrIw',
 ]
 
 s = '''
